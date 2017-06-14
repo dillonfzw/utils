21d1c96aedb657be5a9e2340adb303f031a99123
Zhiwen Fu <dillonfzw@gmail.com>
2017-06-14 08:50:14 +0800

    mirror rtc can login rtc automatically

--- mirror_rtc_2_git.sh
+++ mirror_rtc_2_git.sh
@@ -6,35 +6,46 @@ PROGDIR=${0%/*}
 PROGVERSION=0.1.0
 
 # constant variables
-lscm=lscm
+DEFAULT_lscm=lscm
+DEFAULT_JAVA_HOME=${JAVA_HOME:-/opt/ibm/java-x86_64-80}
 
-rtc_repo=https://jazz07.rchland.ibm.com:21443/jazz/
-rtc_user=fuzhiwen@cn.ibm.com
-rtc_passwd_f=$HOME/.ssh/fuzhiwen@cn.ibm.com.rtc_passwd_f
-rtc_stream=dlm_trunk
-rtc_component=dlm
+DEFAULT_rtc_repo=https://jazz07.rchland.ibm.com:21443/jazz/
+DEFAULT_rtc_user=fuzhiwen@cn.ibm.com
+DEFAULT_rtc_passwd_f=$HOME/.ssh/fuzhiwen@cn.ibm.com.rtc_passwd_f
+DEFAULT_rtc_stream=dlm_trunk
+DEFAULT_rtc_component=dlm
 
-git_repo=git@github.ibm.com:platformcomputing/bluemind.git
-git_branch=rtc-${rtc_stream}
+DEFAULT_git_repo=git@github.ibm.com:platformcomputing/bluemind.git
+DEFAULT_git_branch=rtc-${rtc_stream}
+DEFAULT_workspace=$HOME/workspace
 
-source log.sh
+source $PROGDIR/log.sh
+source $PROGDIR/getopt.sh
 
-workspace=$HOME/workspace
 [ -d $workspace ] || mkdir -p $workspace
+export JAVA_HOME
+export PATH=$JAVA_HOME/bin:$PATH
 
 function rtc_login() {
     local lines=''
-    for i in `seq 0 1`
+    local i=0
+    while [ $i -lt 2 ];
     do
         lines=`$lscm list connections`
         if echo "$lines" | grep -F -sq "$rtc_repo, $rtc_user,"; then
+            echo "$lines" | sed -e 's/^/>> /g' | log_lines info
             break
 
         elif [ $i -eq 0 ]; then
-            $lscm login -r $rtc_repo -u $rtc_user --password-file $rtc_passwd_f
+            log_info "Login RTC repo \"$rtc_repo\" with user \"$rtc_user\"..."
+            lines=`$lscm login -r $rtc_repo -u $rtc_user --password-file $rtc_passwd_f 2>&1`
+            if [ $? -ne 0 ]; then
+                echo "$lines" | sed -e 's/^/>> /g' | log_lines error
+            fi
         fi
+        ((i+=1))
     done
-    echo $i
+    test $i -lt 2
 }
 
 rtc_login
