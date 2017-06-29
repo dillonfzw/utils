2166fd6fd9b48421147955ce2b0b01ddd0c26130
Zhiwen Fu <dillonfzw@gmail.com>
2017-06-29 18:06:55 +0800

    vpid updates

--- update_vpid.sh
+++ update_vpid.sh
@@ -1,18 +1,32 @@
 #! /bin/bash
 
-source log.sh
+DEFAULT_entrypoint=dsm60
+# san1 has primary MAC 00:e0:4c:7a:e3:a7
+# but synology needs to have 00:11:32 prefix
+DEFAULT_mac1=00:11:32:7a:e3:a7
+DEFAULT_menuentry=bare
+DEFAULT_vid=""
+DEFAULT_pid=""
+DEFAULT_fimg=""
 
-fimg=$1
-if [ ! -f "$fimg" ]; then
-    log_error "Image file \"$fimg\" is NOT a valid file. Abort!"
+source log.sh
+source getopt.sh
+
+if [ ! -f "$fimg" -a ! -b "$fimg" ]; then
+    log_error "Image file \"$fimg\" is neither a valid file nor block device. Abort!"
     exit 1
 fi
 
 function get_fdev() {
     local fin=$1
 
-    [ -f "$fin" ] && \
-    fdev=`df -m $fin | grep -v ^Filesystem | awk '{print $1}'` && \
+    if [ -f "$fin" ]; then
+        fdev=`df -m $fin | grep -v ^Filesystem | awk '{print $1}'`
+    elif [ -b "$fin" ]; then
+        fdev=$fin
+    else
+        false
+    fi && \
     [ -b "$fdev" ] && \
     fdev=/sys/class/block/$(basename `readlink -m $fdev`) && \
     [ -d "$fdev" ] && \
@@ -20,11 +34,6 @@ function get_fdev() {
     sed -e 's/ID_MODEL_ID=/pid=0x/g' -e 's/ID_VENDOR_ID=/vid=0x/g'
 }
 
-[ -n "$entrypoint" ] || \
-entrypoint=${2:-dsm60}
-
-#vid=${vid:-0x0781}
-#pid=${pid:-0x5571}
 [ -z "$vid" -o -z "$pid" ] && \
 line=`get_fdev $fimg` && \
 if [ -n "$line" ]; then
@@ -34,10 +43,8 @@ else
     exit 1
 fi
 
-mac1=${mac1:-00:e0:4c:7a:e3:a7}
 mac1=`echo "$mac1" | sed -e 's/://g' | tr 'a-f' 'A-F'`
 
-menuentry=${menuentry:-bare}
 menuentry_tool='XPEnology Configuration Tool v2.2'
 menuentry_bare='XPEnology DS3615xs 6.0.2-8451.5 Baremetal'
 saved_entry=`eval "echo \\\$menuentry_$menuentry"`
@@ -72,21 +79,26 @@ function dsm52() {
 function dsm60() {
     local rc=1
     local ftmpd=`mktemp -d /tmp/update_vpid.XXXXX`
-    if $sudo mount -o loop,offset=$((2048*512)) $fimg $ftmpd; then
+
+    local mount_options="offset=$((2048*512))"
+    [ ! -b "$fimg" ] && mount_options="loop,$mount_options"
+
+    if $sudo mount -o $mount_options $fimg $ftmpd; then
         $sudo sed -i.bak \
             -e 's/^set vid=.*$/set vid='$vid'/g' \
             -e 's/^set pid=.*$/set pid='$pid'/g' \
             -e 's/^set mac1=.*$/set mac1='$mac1'/g' \
+            -e 's/^set default=.*$/set default=\"'"$saved_entry"'\"/g' \
             $ftmpd/grub/grub.cfg && \
         $sudo cp -f $ftmpd/grub/grubenv $ftmpd/grub/grubenv.bak && \
         $sudo grub-editenv $ftmpd/grub/grubenv set saved_entry="$saved_entry" &&
         rc=0 && \
         for FILE in $ftmpd/grub/{grub.cfg,grubenv}; do
-            diff -u $FILE.bak $FILE
+            diff -u $FILE.bak $FILE | sed -e "s/^/`basename $FILE` >> /g"
         done
 
         # output for debug
-        grep -nHE "^set vid=|^set pid=" $ftmpd/grub/grub.cfg
+        grep -nHE "^set sn=|^set vid=|^set pid=|^set mac[0-9]*=|^set default=" $ftmpd/grub/grub.cfg
         grep -nHE "^saved_entry=" $ftmpd/grub/grubenv
         $sudo umount $ftmpd
     fi
