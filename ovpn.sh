3d46cb5a41c01bc0deee1e3bd18fc51d6089f558
Zhiwen Fu <dillonfzw@gmail.com>
2018-01-30 22:27:08 +0800

    inter

--- ovpn.sh
+++ ovpn.sh
@@ -15,6 +15,7 @@ source utils.sh
 [ "$DEBUG" = "true" ] && set -x
 
 [ `whoami` = 'root' ] || sudo=sudo
+ARCH=${ARCH:-`uname -m`}
 
 DEFAULT_workspace=$HOME/workspace
 DEFAULT_ovpn_server_dir=/etc/openvpn/server
@@ -22,40 +23,69 @@ DEFAULT_ovpn_server_dir=/etc/openvpn/server
 source getopt.sh
 
 function initialize_ovpn_server() {
+    local tmpd=`mktemp -d /tmp/XXXXXXXX`
+
     # install openvpn
     local ovpn_server_dir=$ovpn_server_dir && \
 
-    mkdir -p $ovpn_server_dir && \
-    rsync -av /usr/share/easy-rsa $ovpn_server_dir/ && \
+    rsync -av /usr/share/easy-rsa $tmpd/ && \
+    rsync -av /usr/share/doc/easy-rsa-*/ $tmpd/easy-rsa/ && \
 
     # https://www.digitalocean.com/community/tutorials/how-to-set-up-an-openvpn-server-on-ubuntu-16-04
     # https://www.jianshu.com/p/cd2496dbf5bd
-    if cd $ovpn_server_dir/easy-rsa/2.0; then
-        source vars && \
-        ./build-ca && \
-        ./build-key-server server && \
-        ./build-dh && \
-        openvpn --genkey --secret keys/ta.key && \
-        ./build-key-pass client && \
+    if cd $tmpd/easy-rsa/3.0/; then
+        ./easyrsa init-pki && \
+        ./easyrsa build-ca nopass && \
+        ./easyrsa gen-req server nopass && \
+        ./easyrsa sign-req server server && \
+        ./easyrsa gen-req client nopass && \
+        ./easyrsa sign-req client client && \
+        ./easyrsa gen-dh && \
+        openvpn --genkey --secret pki/ta.key && \
+
         cd - >/dev/null 
+    else
+        false
     fi && \
 
     # config ovpn
-    if cd $ovpn_server_dir; then
-        ln -s easy-rsa/2.0/keys ./keys && \
-        cp -a /usr/share/doc/openvpn-*/sample/sample-config-files/{server,client}.conf ./ && \
-
-        cd - >/dev/null
-    fi && \
+    $sudo rsync -av $tmpd/ $ovpn_server_dir/ && \
+    $sudo ln -s easy-rsa/3.0/pki $ovpn_server_dir/pki && \
+    $sudo cp -a /usr/share/doc/openvpn-*/sample/sample-config-files/{server,client}.conf \
+                $ovpn_server_dir/ && \
+    $sudo sed -i -e 's/^ca /#ca /g' \
+              -e 's/^cert /#cert /g' \
+              -e 's/^key /#key /g' \
+              -e 's/^dh /#dh /g' \
+              -e 's/^tls-auth /#tls-auth /g' $ovpn_server_dir/server.conf && \
+    echo "
+<ca>
+$(<$tmpd/easy-rsa/3.0/pki/ca.crt)
+</ca>
+<cert>
+$(<$tmpd/easy-rsa/3.0/pki/issued/server.crt)
+</cert>
+<key>
+$(<$tmpd/easy-rsa/3.0/pki/private/server.key)
+</key>
+<dh>
+$(<$tmpd/easy-rsa/3.0/pki/dh.pem)
+</dh>
+<tls-auth>
+$(<$tmpd/easy-rsa/3.0/pki/ta.key)
+</tls-auth>
+key-direction 0
+cd /etc/openvpn/server
+" | $sudo tee -a $ovpn_server_dir/server.conf && \
 
     # start service
     do_and_verify \
-       "test -L /etc/openvpn/server.conf"  \
+       "$sudo test -L /etc/openvpn/server.conf"  \
        "$sudo ln -s $ovpn_server_dir/server.conf /etc/openvpn" \
        "true" && \
     $sudo systemctl enable openvpn@server.service && \
     if do_and_verify \
-        "$sudo systemctl status openvpn@server.service | grep -sq 'Active: active'" \
+        "eval $sudo systemctl status openvpn@server.service | grep -sq 'Active: active'" \
         "$sudo systemctl start openvpn@server.service" \
         "sleep 10"; then
         $sudo systemctl status openvpn@server.service | log_lines debug
@@ -63,20 +93,40 @@ function initialize_ovpn_server() {
         log_error "Fail to start openvpn@server.service" | log_lines error
         false
     fi
+
+    rc=$?
+    rm -rf $tmpd
+    (exit $rc)
 }
 function install_pam_google_authenticator() {
     # install libpam-google-authenticator
     if cd $workspace; then
-        git clone https://github.com/google/google-authenticator-libpam.git && \
-        cd google-authenticator-libpam && \
-        ./bootstrap.sh && \
-        ./configure && \
-        make dist && \
-        if [ -d $HOME/rpmbuild/SOURCES ]; then mkdir -p $HOME/rpmbuild/SOURCES; fi && \
-        cp google-authenticator-*.tar.gz $HOME/rpmbuild/SOURCES/ && \
-        rpmbuild -bb ./contrib/rpm.spec && \
-        $sudo yum install ~/rpmbuild/RPMS/$ARCH/google-authenticator-*.rpm && \
-        cd - >/dev/null
+        if [ ! -d google-authenticator-libpam ]; then
+            git clone https://github.com/google/google-authenticator-libpam.git
+        fi && \
+        if cd google-authenticator-libpam; then
+            pkgs=${pkgs}${pkgs:+ }" \
+                rpm:autoconf \
+                rpm:automake \
+                rpm:libtool \
+                rpm:rpm-build \
+            " && \
+            if do_and_verify "pkg_verify $pkgs" "pkg_install $pkgs" "true"; then
+                pkg_list_installed $pkgs
+            else
+                log_error "Fail to install pkgs \"`filter_pkgs $pkgs | xargs`\""
+                false
+            fi && \
+
+            ./bootstrap.sh && \
+            ./configure && \
+            make dist && \
+            if [ ! -d $HOME/rpmbuild/SOURCES ]; then mkdir -p $HOME/rpmbuild/SOURCES; fi && \
+            cp google-authenticator-*.tar.gz $HOME/rpmbuild/SOURCES/ && \
+            rpmbuild -bb ./contrib/rpm.spec && \
+            pkg_install_rpm $HOME/rpmbuild/RPMS/$ARCH/google-authenticator-*.rpm && \
+            cd - >/dev/null
+        fi
     fi
 }
 function setup_route() {
@@ -126,7 +176,15 @@ function main() {
         mkdir -p $workspace
     fi && \
 
-    echo install_pam_google_authenticator && \
+    pkgs=${pkgs}${pkgs:+ }" \
+        rpm:google-authenticator \
+    " && \
+    if do_and_verify "pkg_verify $pkgs" "install_pam_google_authenticator" "true"; then
+        pkg_list_installed $pkgs
+    else
+        log_error "Fail to install google-authenticator"
+        false
+    fi && \
 
     setup_route
 }
