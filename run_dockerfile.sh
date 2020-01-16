a3741e3452e528949aa99217ba812ff3ec481435
Zhiwen Fu <dillonfzw@gmail.com>
2020-01-16 15:15:35 +0800

    支持运行多个关联Dockerfile

--- run_dockerfile.sh
+++ run_dockerfile.sh
@@ -3,23 +3,18 @@
 
 for DOCKER_FILE in $@
 do
-    echo -e "\n\nAppling Dockerfile \"$DOCKER_FILE\"...\n" >&2
+    echo "echo -e \"\n\nAppling Dockerfile \"$DOCKER_FILE\"...\n\" >&2"
     sed \
         -e "s/^FROM  *\(.*\)$/echo FROM \1/g" \
         -e "s/^LABEL *\(.*\)$/echo \1/g" \
         -e "s/^USER  *\(.*\)$/echo USER \1/g" \
         \
-        -e "s/^ARG  *\(.*\)$/declare -gx \1/g" \
+        -e "s/^ARG  *\([^ =]*\)[ =]*\(.*\)$/declare -gx \1=\2/g" \
         -e "s/^ENV  *\([^ =]*\)[ =]\(.*\)$/declare -gx \1=\2/g" \
         \
-        -e "s/^RUN  *\(.*\)$/if \[ \$\? -ne 0 \]; then exit -1; fi \&\& \1/g" \
+        -e "s/^RUN  *\(.*\)$/if \[ \$\? -ne 0 \]; then true || exit -1; fi \&\& \1/g" \
         \
         -e "s/^CMD  *\(.*\)$/\1/g" \
-        $DOCKER_FILE \
-    | source /dev/stdin
-    rc=$?
-    if [ $rc -ne 0 ]; then
-        echo -e "\n\nFail to apply Dockerfile \"$DOCKER_FILE\". Abort!" >&2
-        exit $rc
-    fi
-done
+        $DOCKER_FILE </dev/null
+done \
+| source /dev/stdin
