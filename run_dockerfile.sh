761ad569823937241e3d1f370ce4944bcf65860a
Zhiwen Fu <dillonfzw@gmail.com>
2020-01-16 14:44:16 +0800

    修复一个ENV的特殊形式的bug；增加支持LABEL的dockerfile参数

--- run_dockerfile.sh
+++ run_dockerfile.sh
@@ -3,10 +3,14 @@
 
 sed \
     -e "s/^FROM  *\(.*\)$/echo FROM \1/g" \
-    -e "s/^ARG  *\(.*\)$/declare -gx \1/g" \
+    -e "s/^LABEL *\(.*\)$/echo \1/g" \
     -e "s/^USER  *\(.*\)$/echo USER \1/g" \
-    -e "s/^ENV  *\(.*\)$/declare -gx \1/g" \
+    \
+    -e "s/^ARG  *\(.*\)$/declare -gx \1/g" \
+    -e "s/^ENV  *\([^ =]*\)[ =]\(.*\)$/declare -gx \1=\2/g" \
+    \
     -e "s/^RUN  *\(.*\)$/if \[ \$\? -ne 0 \]; then exit -1; fi \&\& \1/g" \
+    \
     -e "s/^CMD  *\(.*\)$/\1/g" \
     $1 \
 | source /dev/stdin
