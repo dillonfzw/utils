3073bccfa8a732040ca56acfb7f40c0de3f6cbb0
Zhiwen Fu <dillonfzw@gmail.com>
2020-01-17 17:35:51 +0800

    run_dockerfile支持内部嵌套调用

--- run_dockerfile.sh
+++ run_dockerfile.sh
@@ -1,21 +1,27 @@
 #! /usr/bin/env bash
 
 
-for DOCKER_FILE in $@
-do
-    echo
-    echo "echo -e \"\n\nAppling Dockerfile \"$DOCKER_FILE\"...\n\" >&2"
-    sed \
-        -e "s/^FROM  *\(.*\)$/echo FROM \1/g" \
-        -e "s/^LABEL *\(.*\)$/echo \1/g" \
-        -e "s/^USER  *\(.*\)$/echo USER \1/g" \
-        \
-        -e "s/^ARG  *\([^ =]*\)[ =]*\"*\(.*[^\"]\)\"*$/declare -gx \1=\${\1:-\"\2\"}/g" \
-        -e "s/^ENV  *\([^ =]*\)[ =]*\"*\(.*[^\"]\)\"*$/declare -gx \1=\${\1:-\"\2\"}/g" \
-        \
-        -e "s/^RUN  *\(.*\)$/if \[ \$\? -ne 0 \]; then false || exit -1; fi \&\& \1/g" \
-        \
-        -e "s/^CMD  *\(.*\)$/\1/g" \
-        $DOCKER_FILE </dev/null
-done \
-| source /dev/stdin
+function run_dockerfile() {
+    local DOCKER_FILE
+    for DOCKER_FILE in $@
+    do
+        echo
+        echo "echo -e \"\n\nAppling Dockerfile \"$DOCKER_FILE\"...\n\" >&2"
+        sed \
+            -e "s/^FROM  *\(.*\)$/echo FROM \1/g" \
+            -e "s/^LABEL *\(.*\)$/echo \1/g" \
+            -e "s/^USER  *\(.*\)$/echo USER \1/g" \
+            \
+            -e "s/^ARG  *\([^ =]*\)[ =]*\"*\(.*[^\"]\)\"*$/declare -gx \1=\${\1:-\"\2\"}/g" \
+            -e "s/^ENV  *\([^ =]*\)[ =]*\"*\(.*[^\"]\)\"*$/declare -gx \1=\${\1:-\"\2\"}/g" \
+            \
+            -e "s/^RUN  *\(.*\)$/if \[ \$\? -ne 0 \]; then false || exit -1; fi \&\& \1/g" \
+            \
+            -e "s/^CMD  *\(.*\)$/\1/g" \
+            $DOCKER_FILE </dev/null
+    done \
+    | source /dev/stdin
+}
+
+
+run_docker_file $@
