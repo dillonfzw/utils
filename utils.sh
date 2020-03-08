f1e51c0f354677e465a2789f182a7d3d2a4ccb6d
Zhiwen Fu <dillonfzw@gmail.com>
2020-03-08 22:07:21 +0800

    增加install_homebrew_mirror的功能

--- utils.sh
+++ utils.sh
@@ -2485,6 +2485,21 @@ function install_docker_compose_linux() {
     fi
     local rc=$?
 }
+function install_homebrew_mirror() {
+    # https://developer.aliyun.com/mirror/
+    # https://developer.aliyun.com/mirror/homebrew?spm=a2c6h.13651102.0.0.3e221b11OJ5itU
+    # 替换brew.git:
+    cd "$(brew --repo)"
+    git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+    # 替换homebrew-core.git:
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+    # 应用生效
+    brew update
+    # 替换homebrew-bottles:
+    echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bash_profile
+    source ~/.bash_profile
+}
 
 # end of feature functions
 #-------------------------------------------------------------------------------
