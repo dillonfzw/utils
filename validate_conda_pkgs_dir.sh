0d438b16ed51805cdc4aacda8ddcc51dbbf0d30d
Zhiwen Fu <dillonfzw@gmail.com>
2020-03-09 07:35:59 +0800

    测试通过validate_conda_pkgs_dir

--- validate_conda_pkgs_dir.sh
+++ validate_conda_pkgs_dir.sh
@@ -26,25 +26,36 @@ if [ -z "$conda_pkgs_dirs" ]; then
 else
     conda_pkgs_dirs=(`echo "$conda_pkgs_dir" | tr ',' ' '`)
 fi
+declare -p conda_pkgs_dirs
 
 
+#
+# 验证并清除conda_pkgs_dirs中无效的内容
+#
+# input:
+# - conda_pkgs_dir array
+#
 function validate_conda_pkgs_dir() {
-    local conda_pkgs_dir
+    local conda_pkgs_dir=""
     local err_cnt=0
 
-    for conda_pkgs_dir in ${conda_pgks_dirs[@]}
+    for conda_pkgs_dir in ${conda_pkgs_dirs[@]}
     do
+        log_info ""
+        log_info "Process conda_pkgs_dir \"$conda_pkgs_dir\""
+        log_info ""
+
         if [   -n "$conda_pkgs_dir" \
             -a -d "$conda_pkgs_dir" \
             -a -f "$conda_pkgs_dir/urls.txt" \
             -a -f "$conda_pkgs_dir/urls" \
-        ]: then
+        ]; then
             true;
         else
             continue;
         fi \
      && if cd $conda_pkgs_dir; then
-            for FILE in *.bz2; \
+            for FILE in `ls -1d *.bz2 2>/dev/null`; \
             do \
                 if [ -f $FILE.SHA1SUM ]; then \
                     true; \
@@ -53,8 +64,8 @@ function validate_conda_pkgs_dir() {
                         sha1sum -b $FILE | tee $FILE.SHA1SUM \
                      && true; \
                     else \
-                        log_warn "$FILE fail" \
-                     && echo rm -f $FILE \
+                        log_warn "Fail to verify $FILE" \
+                     && rm -f $FILE \
                      && true; \
                     fi \
                  && true; \
@@ -71,7 +82,8 @@ function validate_conda_pkgs_dir() {
                     true; \
                 else \
                     log_info "rm obsolete $FILE" \
-                 && echo rm -rf $FILE \
+                 && rm -rf $FILE \
+                 && { rm -f $FILE.* 2>/dev/null || true; } \
                  && true;
                 fi \
              && true;
@@ -84,10 +96,11 @@ function validate_conda_pkgs_dir() {
             log_error "Cannot enter pkgs_dir \"$pkgs_dir\""
             ((err_cnt+=1))
             break
-        fi
+        fi;
+        true;
     done \
  && test $err_cnt -eq 0 \
- && true; \
+ && true;
 }
 
 
