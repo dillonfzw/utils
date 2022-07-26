b851579f08d4f329549a5bbd65ef09cd3752bbd0
Zhiwen Fu <dillonfzw@gmail.com>
2022-07-26 09:12:28 +0800

    support install anaconda in other arch

--- utils.sh
+++ utils.sh
@@ -2574,8 +2574,8 @@ DEFAULT_python_ver_major=${DEFAULT_python_ver_major:-${python_ver_major:-"3"}}
 DEFAULT_conda_install_home=${DEFAULT_conda_install_home:-"$HOME/anaconda${DEFAULT_python_ver_major}"}
 DEFAULT_conda_env_name=${DEFAULT_conda_env_name:-"base"}
 DEFAULT_conda_envs_dir=${DEFAULT_conda_envs_dir:-"$HOME/.conda/envs"}
-# https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-DEFAULT_conda_installer_url=${DEFAULT_conda_installer_url:-"https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh"}
+# https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-${ARCH:-`uname -m`}.sh
+DEFAULT_conda_installer_url=${DEFAULT_conda_installer_url:-"https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-${ARCH:-`uname -m`}.sh"}
 DEFAULT_always_force_activating_conda_env=${DEFAULT_always_force_activating_conda_env:${always_force_activating_conda_env:-true}}
 function install_nginx_prereqs_on_ubuntu() {
     local -a pkgs=(
