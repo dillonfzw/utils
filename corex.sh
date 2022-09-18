13129e7b49f51637225f17816a2c2546d9fa244c
Zhiwen Fu <dillonfzw@gmail.com>
2022-09-18 04:36:17 +0000

    support general corex and sw_home

--- corex.sh
+++ corex.sh
@@ -2,23 +2,36 @@
 
 
 if true; then true \
- && COREX_HOME_uy7rohTh=${COREX_HOME:-/usr/local/corex} \
  && declare -a _extra_bin_path_uy7rohTh=() \
  && declare -a _extra_lib_path_uy7rohTh=() \
- && if [ -d "${COREX_HOME_uy7rohTh}" ] && \
-         ! echo $PATH | tr ':' '\n' | grep -xF $COREX_HOME_uy7rohTh/bin; then true \
-     && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/bin") \
-     && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/lib64") \
-     && true; \
-    fi \
+ && for COREX_HOME_uy7rohTh in ${COREX_HOME} $HOME/workspace/sw_home /usr/local/corex /opt/sw_home; \
+    do true \
+     && if [ -z "${COREX_HOME_uy7rohTh}" -o ! -d "${COREX_HOME_uy7rohTh}/" ]; then true \
+         && continue; \
+        elif echo "${COREX_HOME_uy7rohTh}" | grep -sq "/sw_home[/]*$"; then true \
+         && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/bin") \
+         && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/cuda/bin") \
+         && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/lib64") \
+         && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/cuda/lib64") \
+         && true; \
+        else true \
+         && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/bin") \
+         && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/lib64") \
+         && true; \
+        fi \
+     && break; \
+    done \
  && COREX_CMAKE_HOME_uy7rohTh=`ls -1d {/usr/local,/opt}/cmake-*-corex* 2>/dev/null | sort -V -r | head -n1` \
- && if [ "x${COREX_CMAKE_HOME_uy7rohTh}" != "x" ] && \
-         ! echo $PATH | tr ':' '\n' | grep -xF $COREX_CMAKE_HOME_uy7rohTh/bin; then true \
+ && if [ "x${COREX_CMAKE_HOME_uy7rohTh}" != "x" ]; then true \
      && _extra_bin_path_uy7rohTh+=("${COREX_CMAKE_HOME_uy7rohTh}/bin") \
      && true; \
     fi \
- && export PATH=`echo "${_extra_bin_path_uy7rohTh[@]}" | tr ' ' ':'`${PATH:+:${PATH}} \
- && export LD_LIBRARY_PATH=`echo "${_extra_lib_path_uy7rohTh[@]}" | tr ' ' ':'`${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} \
+ && PATH=`echo "${_extra_bin_path_uy7rohTh[@]}" | tr ' ' ':'`${PATH:+:${PATH}} \
+ && PATH=`echo "${PATH}" | tr ':' '\n' | nl -nln -w1 -s'|' | sort -t'|' -k2,2 -u | sort -t'|' -k1 -n | cut -d'|' -f2 | tr '\n' ':' | sed -e 's/^:\(.*\):/\1/g'` \
+ && LD_LIBRARY_PATH=`echo "${_extra_lib_path_uy7rohTh[@]}" | tr ' ' ':'`${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} \
+ && LD_LIBRARY_PATH=`echo "${LD_LIBRARY_PATH}" | tr ':' '\n' | nl -nln -w1 -s'|' | sort -t'|' -k2,2 -u | sort -t'|' -k1 -n | cut -d'|' -f2 | tr '\n' ':' | sed -e 's/^:\(.*\):/\1/g'` \
+ && export PATH \
+ && export LD_LIBRARY_PATH \
  && unset _extra_bin_path_uy7rohTh \
  && unset _extra_lib_path_uy7rohTh \
  && unset COREX_HOME_uy7rohTh \
