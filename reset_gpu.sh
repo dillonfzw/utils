b48b36ecd228c655ad90c44ffb9c2c86e3a99436
Zhiwen Fu <dillonfzw@gmail.com>
2022-11-26 19:07:17 +0800

    支持gpu_reset

---
+++ reset_gpu.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+
+function reset_gpu_iluvatar() { ixsmi -r -i ${1}; }
+function show_gpu_iluvatar() { ixsmi ${1:+"-i"} ${1} | grep -B99999 "^ *$"; }
+
+function reset_gpu_nvidia() { nvidia -r -i ${1}; }
+function show_gpu_nvidia() { nvidia ${1:+"-i"} ${1} | grep -B99999 "^ *$"; }
+
+function reset_gpu_service() {
+    local gpu_type=${1:-iluvatar} \
+ && local reset_gpu=reset_gpu_${gpu_type} \
+ && local show_gpu=show_gpu_${gpu_type} \
+ && while true; \
+    do true \
+     && ${show_gpu} \
+     && printf "Please input target gpu index: " \
+     && read GPU_ID \
+     && ${show_gpu} ${GPU_ID} \
+     && printf "Confirm your selected gpu, \"YES\" or \"NO\": " \
+     && read YESNO \
+     && if [ "x${YESNO}" = "xYES" ]; then true \
+         && if ${reset_gpu} ${GPU_ID}; then true \
+             && echo "[I]: reset ${gpu_type} gpu ${GPU_ID} successfully!" \
+             && true; \
+            else true \
+             && echo "[W]: reset ${gpu_type} gpu ${GPU_ID} failed!" \
+             && true; \
+            fi \
+         && dmesg -H | tail -n10 | sed -e 's/^/[kernel] >> /g' \
+         && printf "Press any key to continue." \
+         && read _ \
+         && clear \
+         && true; \
+        fi \
+     && true; \
+    done \
+ && true; \
+}
+
+
+reset_gpu_service
