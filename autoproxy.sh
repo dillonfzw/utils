884eca61b61a7d03dcc56e21e587eada0c4a7372
Zhiwen Fu <dillonfzw@gmail.com>
2022-12-09 09:02:12 +0800

    extra fwd

--- autoproxy.sh
+++ autoproxy.sh
@@ -5,56 +5,56 @@ source ~/bin/log.sh
 source ~/bin/utils.sh
 
 
-function test_port() {
-    nc -zv $1 $2 2>&1 | grep -sq succeeded
-}
-function start_socks_7070() { autossh -M 20010 -f seulogin.2alice.site       -D7070 -gfCN -t -e none; }
-function start_socks_7071() { autossh -M 20012 -f fuzwvps1.2alice.site       -D7071 -gfCN -t -e none -o 'ProxyCommand nc -x localhost:7070 %h %p'; }
-function start_socks_7072() { autossh -M 20014 -f vpn2c-bladesk1.2alice.site -D7072 -gfCN -t -e none; }
-function start_socks_7077() { autossh -M 20020 -f localhost                  -D7077 -gfCN -t -e none; }
-
-
 HOST_NAME_s=`hostname -s`
 
 
-if do_and_verify \
-    "test_port localhost 7077" \
-    start_socks_7077 \
-    "sleep 5"; then true \
- && log_info "socks proxy 7077 was ready!" \
+function  start_socks_7070() { autossh -M 20010 -f seulogin.2alice.site       -D7070 -gfCN -t -e none; }
+function verify_socks_7070() { test_port localhost 7070; }
+function  start_socks_7071() { autossh -M 20012 -f fuzwvps1.2alice.site       -D7071 -gfCN -t -e none -o 'ProxyCommand nc -x localhost:7070 %h %p'; }
+function verify_socks_7071() { test_port localhost 7071; }
+function  start_socks_7072() { autossh -M 20014 -f vpn2c-bladesk1.2alice.site -D7072 -gfCN -t -e none; }
+function verify_socks_7072() { test_port localhost 7072; }
+function  start_socks_7077() { autossh -M 20020 -f localhost                  -D7077 -gfCN -t -e none; }
+function verify_socks_7077() { test_port localhost 7077; }
+function  start_fwd01_7077() { autossh -M 20022 -f mr-n1-dmz                  -R7077:localhost:7077 -gfCN -t -e none; }
+function verify_fwd01_7077() { test_port localhost 20022; }
+function  start_fwd02_7077() { autossh -M 20024 -f bj-209-20-22-fzw-mr1       -R7077:localhost:7077 -gfCN -t -e none; }
+function verify_fwd02_7077() { test_port localhost 20024; }
+function test_port() { nc -zv $1 $2 2>&1 | grep -sq succeeded; }
+function run_job() {
+    if [ ${#@} -eq 0 ]; then return; fi
+    local job=$1
+    local title=${2:-${job}}
+    if do_and_verify \
+        "verify_${job}" \
+        "start_${job}" \
+        "sleep 5"; then true \
+     && log_info "\"${HOST_NAME_s}\": Successfully ${title}!" \
+     && true; \
+    else true \
+     && log_error "\"${HOST_NAME_s}\": Fail to ${title}" \
+     && false; \
+    fi
+}
+function main() {
+    true \
+ && if true; then true \
+     && run_job socks_7077 "start self proxy at 7077" \
+     && run_job socks_7070 "start tunnel to seu at 7070" \
+     && run_job socks_7071 "start tunnel to gfw at 7071" \
+     && true; \
+    fi \
+ && if [ "$HOST_NAME_s" == "blahome" ]; then true \
+     && run_job socks_7072 "start tunnel to bj office at 7072" \
+     && true; \
+    fi \
+ && if [ "$HOST_NAME_s" == "bladesk1" ]; then true \
+     && run_job fwd01_7077 "forward 7077 to bj-209-20-22" \
+     && run_job fwd02_7077 "forward 7077 to bj-209-20-22-fzw-mr1" \
+     && true; \
+    fi \
  && true; \
-else true \
- && log_error "Fail to start socks proxy at 7077" \
- && false; \
-fi && \
-if do_and_verify \
-    "test_port localhost 7070" \
-    start_socks_7070 \
-    "sleep 5"; then true \
- && log_info "socks proxy 7070 was ready!" \
- && true; \
-else true \
- && log_error "Fail to start socks proxy at 7070" \
- && false; \
-fi && \
-if do_and_verify \
-    "test_port localhost 7071" \
-    start_socks_7071 \
-    "sleep 5"; then true \
- && log_info "socks proxy 7071 was ready!" \
- && true; \
-else true \
- && log_error "Fail to start socks proxy at 7071" \
- && false; \
-fi && \
-if [ "$HOST_NAME_s" == "blahome" ] && if do_and_verify \
-    "test_port localhost 7072" \
-    start_socks_7072 \
-    "sleep 5"; then true \
- && log_info "socks proxy 7072 was ready!" \
- && true; \
-else true \
- && log_error "Fail to start socks proxy at 7072" \
- && false; \
-fi; then true; fi && \
-true
+}
+
+
+main $@
