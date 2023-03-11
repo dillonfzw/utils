fffd82d1c8fb962daf2f598a4e61277b4c9f997e
Zhiwen Fu <dillonfzw@gmail.com>
2023-03-11 11:22:00 +0800

    enhance cuda_map_run.sh for log

--- cuda_map_run.sh
+++ cuda_map_run.sh
@@ -7,6 +7,26 @@ _WRAPPED_CMD=$1; shift
 TS=`date "+%Y%m%d_%H%M"`
 
 
+#
+# prepare help usage
+#
+function help() {
+    echo "Usage: $0 0,2,3,4 /foo/bar/start.sh parm1 parm2 parm3" >&2
+    echo "..." >&2
+    echo "" >&2
+    echo "$ ls logs/log.*.txt" >&2
+    echo "logs/log.start-2023XXXX_XXXX-d0.txt" >&2
+    echo "logs/log.start-2023XXXX_XXXX-d2.txt" >&2
+    echo "logs/log.start-2023XXXX_XXXX-d3.txt" >&2
+    echo "logs/log.start-2023XXXX_XXXX-d4.txt" >&2
+    exit 0
+}
+if [ -z "${_WRAPPED_CMD}" ]; then help; exit 0; fi
+
+
+#
+# prepare house cleaner
+#
 declare -a G_task_pids=()
 function finish() {
     if [ ${#G_task_pids[@]} -eq 0 ]; then return; fi
@@ -22,7 +42,9 @@ function finish() {
 trap finish SIGTERM SIGINT
 
 
-
+#
+# run workloads in target cuda devices
+#
 [ -d logs ] || mkdir logs
 for _CUDA_DEVICE in `echo ${_CUDA_DEVICES} | tr ',' ' '`
 do
@@ -34,4 +56,21 @@ do
 done
 
 
-wait
+#
+# show aggregated output
+#
+tail -F logs/log.${_WRAPPED_CMD}-${TS}-d*.txt &
+G_task_wait=$!
+
+
+#
+# wait all tasks' finish
+#
+wait ${G_task_pids[@]}
+
+
+#
+# terminate output
+#
+kill -SIGINT ${G_task_wait}
+exit 0
