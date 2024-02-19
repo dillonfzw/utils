af80dbe126f823c4edb7f7cf996577efc9953876
Zhiwen Fu <dillonfzw@gmail.com>
2024-02-20 00:55:23 +0800

    COREX_HOME needs to be validated and pick the 1st valid one

--- corex.sh
+++ corex.sh
@@ -15,24 +15,24 @@ if true; then true \
  && declare -a _extra_bin_path_uy7rohTh=() \
  && declare -a _extra_lib_path_uy7rohTh=() \
  && declare -a _extra_inc_path_uy7rohTh=() \
- && for COREX_HOME_uy7rohTh in ${COREX_HOME} $HOME/workspace/sw_home /usr/local/corex `ls -1d /usr/local/corex-* 2>/dev/null | sort -V | tail -n1` /opt/sw_home; \
+ && for COREX_HOME_uy7rohTh in ${COREX_HOME} $HOME/workspace/sw_home /usr/local/corex `ls -1d /usr/local/corex-* 2>/dev/null | sort -V -r` /opt/sw_home; \
     do true \
      && if [ -z "${COREX_HOME_uy7rohTh}" -o ! -d "${COREX_HOME_uy7rohTh}/" ]; then true \
          && continue; \
-        elif echo "${COREX_HOME_uy7rohTh}" | grep -sq "/sw_home[/]*$"; then true \
+        elif echo "${COREX_HOME_uy7rohTh}" | grep -sq "/sw_home[/]*$" && [ -x ${COREX_HOME_uy7rohTh}/local/bin/ixsmi ]; then true \
          && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/bin") \
          && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/cuda/bin") \
          && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/lib64") \
          && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/cuda/lib64") \
          && _extra_inc_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/local/cuda/include") \
          && true; \
-        else true \
+        elif [ -x ${COREX_HOME_uy7rohTh}/bin/ixsmi ]; then true \
          && _extra_bin_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/bin") \
          && _extra_lib_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/lib64") \
          && _extra_inc_path_uy7rohTh+=("${COREX_HOME_uy7rohTh}/include") \
          && true; \
         fi \
-     && break; \
+     && true; \
     done \
  && COREX_CMAKE_HOME_uy7rohTh=`ls -1d {/usr/local,/opt}/cmake-*-corex* 2>/dev/null | sort -V -r | head -n1` \
  && if [ "x${COREX_CMAKE_HOME_uy7rohTh}" != "x" ]; then true \
